ac189b23d062aca2c512be286f463f6c5b9187a1
Michael Robinson <seraph890@gmail.com>
2016-09-29 21:09:40 +1000

    fixed flatten to work correctly, added comments

--- flatten/index.js
+++ flatten/index.js
@@ -1,18 +1,24 @@
-module.exports = function flatten(arr) {
+var flatten_array = function flatten(arr, item) {
   // Array to store our array as its being flattened
-  item = []
+  var item = item || [];
 
   // Loop through our array
   for (var i = 0, len = arr.length; i < len; i++) {
 
     // Check to see if the current position in the array is of type "array"/
     // Calls self until there are no more arrays to loop through
-    if (arr[i].isArray) {
-      flatten(arr[i]);
+    if ( Array.isArray(arr[i]) ) {
+
+      // Call function and send current results back through the loop
+      flatten_array(arr[i], item);
     } else {
+
+      // Push current non-array into items
       item.push(arr[i]);
     }
   }
 
   return item;
 };
+
+module.exports = flatten_array;
