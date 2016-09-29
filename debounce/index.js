930ca16c836c3ff3f6df607aa0318a1f6196defd
Michael Robinson <seraph890@gmail.com>
2016-09-29 22:24:54 +1000

    simplified debounce

--- debounce/index.js
+++ debounce/index.js
@@ -1,11 +1,14 @@
-var debouncer = function debounce(wait) {
+var debouncer = function debounce(func, wait) {
 	var timeout;
-	var func = this;
 
 	return function() {
-		var context = this, args = arguments;
+		var context = this
+		var args = arguments;
+
 		clearTimeout(timeout);
+
 		timeout = setTimeout(function() {
+
 		    func.apply(context, args);
 		}, wait || 100);
 	};
